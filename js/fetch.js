20378933de3c9d5ba394a8f64dfb6062a5f5a792
Manuel Hernandez <manuel_alejandro090401@outlook.com>
2022-11-04 12:57:55 -0700

    update

---
+++ js/fetch.js
@@ -0,0 +1,14 @@
+var formdata = new FormData();
+formdata.append("email", "mdiaz_19@alu.uabcs.mx");
+formdata.append("password", "d84#6LW63KUpRz");
+
+var requestOptions = {
+  method: 'POST',
+  body: formdata,
+  redirect: 'follow'
+};
+
+fetch("https://crud.jonathansoto.mx/api/login", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
---
+++ js/script.js
@@ -0,0 +1,5 @@
+// if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker.register('../sw.js')
+//       .then(reg => console.log('Registro de SW exitoso'))
+//       .catch(err => console.log('El sw ya se encuentra creado'))
+//   }

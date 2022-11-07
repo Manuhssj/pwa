42e8bacc3a813cbdba400662f9169c90e048f0dd
Manuel Hernandez <manuel_alejandro090401@outlook.com>
2022-11-07 13:31:11 -0700

    update

--- sw.js
+++ sw.js
@@ -1,75 +1,70 @@
 ;
-// Asignar un nombre y version al cache
-// const CACHE_NAME='v1_prueba_pwa',urlsToCache = [
-//     './',
-//     './css/style.css',
-//     './js/app.js',
-//     './img/icon_1024.png',
-//     './img/icon_512.png',
-//     './img/icon_384.png',
-//     './img/icon_256.png',
-//     './img/icon_192.png',
-//     './img/icon_128.png',
-//     './img/icon_96.png',
-//     './img/icon_64.png',
-//     './img/icon_32.png',
-//     './img/icon_16.png',
-//     './img/ios_96.jpg'
-    
-
-
-// ]
+//Asignar un nombre y version al cache
+const CACHE_NAME='v1_prueba_pwa',urlsToCache = [
+    './',
+    './productos',
+    './productos/index.html',
+    './css/style.css',
+    './js/app.js',
+    './img/icon_1024.png',
+    './img/icon_512.png',
+    './img/icon_384.png',
+    './img/icon_256.png',
+    './img/icon_192.png',
+    './img/icon_128.png',
+    './img/icon_96.png',
+    './img/icon_64.png',
+    './img/icon_32.png',
+    './img/icon_16.png',
+    './img/ios_96.jpg'
+]
 
 //durante la fase de instalación, generalmente se almacena en caché los activos estáticos
-// self.addEventListener('install', e => {
-//   e.waitUntil(
-//     caches.open(CACHE_NAME)
-//       .then(cache => {
-//         return cache.addAll(urlsToCache)
-//           .then(() => self.skipWaiting())
-//       })
-//       .catch(err => console.log('Falló registro de cache', err))
-//   )
-// })
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache)
+          .then(() => self.skipWaiting())
+      })
+      .catch(err => console.log('Falló registro de cache', err))
+  )
+})
 
-// //una vez que se instala el SW, se activa y busca los recursos para hacer que funcione sin conexión
-// self.addEventListener('activate', e => {
-//   const cacheWhitelist = [CACHE_NAME]
+//una vez que se instala el SW, se activa y busca los recursos para hacer que funcione sin conexión
+self.addEventListener('activate', e => {
+  const cacheWhitelist = [CACHE_NAME]
 
-//   e.waitUntil(
-//     caches.keys()
-//       .then(cacheNames => {
-//         return Promise.all(
-//           cacheNames.map(cacheName => {
-//             //Eliminamos lo que ya no se necesita en cache
-//             if (cacheWhitelist.indexOf(cacheName) === -1) {
-//               return caches.delete(cacheName)
-//             }
-//           })
-//         )
-//       })
-//       // Le indica al SW activar el cache actual
-//       .then(() => self.clients.claim())
-//   )
-// })
+  e.waitUntil(
+    caches.keys()
+      .then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            //Eliminamos lo que ya no se necesita en cache
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName)
+            }
+          })
+        )
+      })
+      // Le indica al SW activar el cache actual
+      .then(() => self.clients.claim())
+  )
+})
 
 //cuando el navegador recupera una url
-// self.addEventListener('fetch', e => {
-//   //Responder ya sea con el objeto en caché o continuar y buscar la url real
-//   e.respondWith(
-//     caches.match(e.request)
-//       .then(res => {
-//         if (res) {
-//           //recuperar del cache
-//           return res
-//         }
-//         //recuperar de la petición a la url
-//         return fetch(e.request)
-//       })
-//   )
-//   // console.log(e);
-// })
-
-self.addEventListener('fetch', event=>{
-  event.respondWith( fetch( fetch( event.request ) ) );
+self.addEventListener('fetch', e => {
+  //Responder ya sea con el objeto en caché o continuar y buscar la url real
+  e.respondWith(
+    caches.match(e.request)
+      .then(res => {
+        if (res) {
+          //recuperar del cache
+          return res
+        }
+        //recuperar de la petición a la url
+        return fetch(e.request)
+      })
+  )
+  console.log(e);
 })

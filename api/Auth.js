2c85cd341e0ec74040325c4fd1cdac53dacdca5d
Manuel Hernandez <manuel_alejandro090401@outlook.com>
2022-11-08 13:53:43 -0700

    update

--- api/Auth.js
+++ api/Auth.js
@@ -1,43 +1,43 @@
-function mostrarContrasena(){
-    var tipo = document.getElementById("password");
-    if(tipo.type == "password"){
-        tipo.type = "text";
-    }else{
-        tipo.type = "password";
-    }
-}
+// function mostrarContrasena(){
+//     var tipo = document.getElementById("password");
+//     if(tipo.type == "password"){
+//         tipo.type = "text";
+//     }else{
+//         tipo.type = "password";
+//     }
+// }
 
-const myForm = document.getElementById('myForm');
-myForm.addEventListener('submit', function(e){
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+// const myForm = document.getElementById('myForm');
+// myForm.addEventListener('submit', function(e){
+//     const email = document.getElementById('email').value;
+//     const password = document.getElementById('password').value;
 
-    e.preventDefault();
+//     e.preventDefault();
 
-    var formdata = new FormData();
-    formdata.append("email", email);
-    formdata.append("password", password);
+//     var formdata = new FormData();
+//     formdata.append("email", email);
+//     formdata.append("password", password);
     
-    var requestOptions = {
-      method: 'POST',
-      body: formdata,
-      redirect: 'follow'
-    };
+//     var requestOptions = {
+//       method: 'POST',
+//       body: formdata,
+//       redirect: 'follow'
+//     };
     
-    fetch("https://crud.jonathansoto.mx/api/login", requestOptions)
-      .then(response => response.json())
-      .then(result => {
-        const {code} = result
-        if(code === 2){
-          localStorage.setItem("token", JSON.stringify( result ) )
-          window.location.href="../productos/index.html";
-        }else{
-          alert("El email o contraseña son incorrectas")
-        }
-      })
-      .catch(error => console.log('error', error));
+//     fetch("https://crud.jonathansoto.mx/api/login", requestOptions)
+//       .then(response => response.json())
+//       .then(result => {
+//         const {code} = result
+//         if(code === 2){
+//           localStorage.setItem("token", JSON.stringify( result ) )
+//           window.location.href="../productos/index.html";
+//         }else{
+//           alert("El email o contraseña son incorrectas")
+//         }
+//       })
+//       .catch(error => console.log('error', error));
     
 
-});
+// });
 
 
--- api/ProductController.js
+++ api/ProductController.js
@@ -1,49 +1,49 @@
-var carrito = [];
-let local = JSON.parse(localStorage.getItem("token"));
+// var carrito = [];
+// let local = JSON.parse(localStorage.getItem("token"));
 
 
-let myHeaders = new Headers();
-myHeaders.append("Authorization", `Bearer ${local.data.token}`);
-let requestOptions = {
-    method: 'GET',
-    headers: myHeaders,
-    redirect: 'follow'
-};
-fetch("https://crud.jonathansoto.mx/api/products", requestOptions)
-    .then(response => response.json())
-    .then(data => mostrarDatos(data.data))
-    .catch(error => console.log('error', error));
+// let myHeaders = new Headers();
+// myHeaders.append("Authorization", `Bearer ${local.data.token}`);
+// let requestOptions = {
+//     method: 'GET',
+//     headers: myHeaders,
+//     redirect: 'follow'
+// };
+// fetch("https://crud.jonathansoto.mx/api/products", requestOptions)
+//     .then(response => response.json())
+//     .then(data => mostrarDatos(data.data))
+//     .catch(error => console.log('error', error));
 
-const mostrarDatos = (data) =>{
+// const mostrarDatos = (data) =>{
     
-    let body = ''
-    for (let i = 0; i < data.length; i++) {
-        body += `<div class="m-3">
-                            <div class="row row-cols-1 row-cols-md-2 row-cols-sm-1 row-cols-lg-3 row-cols-xl-4 row-cols-xxl-5 g-4">
-                                <div class="col">
-                                    <div class="card">
-                                        <img src="${data[i].cover}" class="card-img-top" alt="...">
-                                        <div class="card-body">
-                                            <h5 class="card-title">${data[i].name}</h5>
-                                            <p class="card-text">${data[i].description}</p> 
-                                        </div>
-                                        <div class="card-footer">
-                                            <button class="btn btn-info col-12">
-                                                Ver mas
-                                            </button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>`
+//     let body = ''
+//     for (let i = 0; i < data.length; i++) {
+//         body += `<div class="m-3">
+//                             <div class="row row-cols-1 row-cols-md-2 row-cols-sm-1 row-cols-lg-3 row-cols-xl-4 row-cols-xxl-5 g-4">
+//                                 <div class="col">
+//                                     <div class="card">
+//                                         <img src="${data[i].cover}" class="card-img-top" alt="...">
+//                                         <div class="card-body">
+//                                             <h5 class="card-title">${data[i].name}</h5>
+//                                             <p class="card-text">${data[i].description}</p> 
+//                                         </div>
+//                                         <div class="card-footer">
+//                                             <button class="btn btn-info col-12">
+//                                                 Ver mas
+//                                             </button>
+//                                         </div>
+//                                     </div>
+//                                 </div>
+//                             </div>
+//                         </div>`
                         
-        document.getElementById("cards").innerHTML=body;
+//         document.getElementById("cards").innerHTML=body;
 
         
-    }
-}
+//     }
+// }
 
-const agregarAlCarrito = (prodId) => {
-    // const item = stockProductos.find((prod) => data.id === prod.id)
-}
-console.log(data);
+// const agregarAlCarrito = (prodId) => {
+//     // const item = stockProductos.find((prod) => data.id === prod.id)
+// }
+// console.log(data);

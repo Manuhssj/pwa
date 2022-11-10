b09f58b6843d85900baa6fe89c026ebfdc716108
Manuel Alejandro Diaz Hernandez <Manuel_alejandro090401@outlook.com>
2022-11-09 20:03:44 -0700

    update

--- sw.js
+++ sw.js
@@ -1,74 +1,45 @@
-;
-//Asignar un nombre y version al cache
-const CACHE_NAME='v1_prueba_pwa',urlsToCache = [
-    './',
-    './api/Auth.js',
-    './api/ProductController.js',
-    './css/style.css',
-    './productos/index.html',
-    './img/icon_1024.png',
-    './img/icon_512.png',
-    './img/icon_384.png',
-    './img/icon_256.png',
-    './img/icon_192.png',
-    './img/icon_128.png',
-    './img/icon_96.png',
-    './img/icon_64.png',
-    './img/icon_32.png',
-    './img/icon_16.png',
-    './img/ios_96.jpg',
-    './index.html',
-    './.jshintrc',
-    './sw.js',
-    './swRegister.js',
-  ]
-  
-  
-  //Durante la fase de instalación, generalmente se almacena en caché los activos estáticos
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME)
-    .then(cache => {
-      return cache.addAll(urlsToCache)
-      .then(() => self.skipWaiting())
-    })
-    .catch(err => console.log('Falló registro de cache', err))
-    )
-  })
-  
-  //una vez que se instala el SW, se activa y busca los recursos para hacer que funcione sin conexión
-  self.addEventListener('activate', e => {
-    const cacheWhitelist = [CACHE_NAME]
+const staticCacheName = 'site-static-v2';
+const  assets = [
+    '/fallback/fallback.html',
+];
+
+
+// Instalacion del service worker
+self.addEventListener('install', event => {
+    // console.log('Service worker ha sido instalado');
+    event.waitUntil(caches.open(staticCacheName).then(cache => {
+        console.log("cachando");
+        cache.addAll(assets); 
+    }));
     
-    e.waitUntil(
-      caches.keys()
-      .then(cacheNames => {
-        return Promise.all(
-          cacheNames.map(cacheName => {
-            //Eliminamos lo que ya no se necesita en cache
-            if (cacheWhitelist.indexOf(cacheName) === -1) {
-              return caches.delete(cacheName)
-            }
-          })
-        )
-      })
-      // Le indica al SW activar el cache actual
-      .then(() => self.clients.claim())
-      )
 })
 
-//cuando el navegador recupera una url
-self.addEventListener('fetch', e => {
-  //Responder ya sea con el objeto en caché o continuar y buscar la url real
-  e.respondWith(
-      caches.match(e.request)
-        .then(res => {
-            if (res) {
-          //recuperar del cache
-          return res
-        }
-        //recuperar de la petición a la url
-        return fetch(e.request)
-      })
-  )
+// Activacion del service worker
+self.addEventListener('activate', event => {
+    // console.log("Service worker ha sido activado");
+    event.waitUntil(
+        caches.keys().then(keys => {
+            // console.log(keys);
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName)
+                .map(key => caches.delete(key))
+            )
+        })
+    )
+})
+
+// Fetch event
+
+self.addEventListener('fetch', event => {
+    // console.log('Fetch evento',event);
+    event.respondWith(
+        caches.match(event.request).then(cacheRes => {
+                return cacheRes || fetch(event.request).then(fetchRes =>{
+                    return caches.open(dynamicCacheName).then(cache => {
+                        cache.put(event.request.url, fetchRes.clone());
+                        return fetchRes;
+                    })
+                })
+            }).catch(() => caches.match('/fallback/fallback.html'))
+    )
 })
--- swRegister.js
+++
@@ -1,5 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js')
-      .then(reg => console.log('Registro de SW exitoso'))
-      .catch(err => console.log('El sw ya se encuentra creado'))
-  }
